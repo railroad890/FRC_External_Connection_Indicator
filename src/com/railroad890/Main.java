4195e188e14a86f5aae8ee1bfed2ad518d4e24b4
railroad890 <jamestrains890@gmail.com>
2018-03-22 11:24:51 -0400

    added main code

--- src/com/railroad890/Main.java
+++ src/com/railroad890/Main.java
@@ -1,7 +1,23 @@
 package com.railroad890;
 
 public class Main {
+
     public static void main(String[] args){
+
+        GetConnection _connection = new GetConnection();
+        Output _output = new Output(/*arduino port*/);
+
+        boolean isConnected = _connection.getIsConnected();
+        _output.openConnection();
+
+        while (true){
+            isConnected = _connection.getIsConnected();
+            if(isConnected){
+                _output.sendString("connected");
+            }else{
+                _output.sendString("disconnected");
+            }
+        }
 
     }
 }
